01033539c9d8e1963f15f856f61d980e656cdfa7
Aaron Camilo <Aaron@Mildreds-MacBook-Pro-2.local>
2022-09-29 18:39:42 -0400

    classnotes

---
+++ week1/workW1D4.js
@@ -0,0 +1,11 @@
+// let arr =['c','a','t']
+// let str = "cat"
+// str[0] = "x"
+// console.log(str)
+// arr[0]="x"
+// console.log(arr)
+
+let arr =['h',10,9,true,undefined,null,[1,2]]
+console.log(arr)
+console.log(arr[0])
+console.log(arr[6][1])
