d7b81fa371a997ac5dafc469efb3b7f2064be5d3
Aaron Camilo <Aaron@Mildreds-MacBook-Pro-2.local>
2022-10-07 00:06:10 -0400

    problemset

---
+++ week2/w2d4_pset/problem_set/problems/5_debug_5.js
@@ -0,0 +1,23 @@
+// Debug this function below so it passes mocha
+
+// Examples:
+//
+// fizzBuzz(19) //=> [3, 5, 6, 9, 10, 12, 18]
+
+function fizzBuzz(max) {
+  let i = 1;
+  let array = [];
+
+  while(i < max) {
+    if ((i % 5 === 0 || i % 3 === 0) && i % 15 !== 0) {
+      array.push(i);
+    }
+    i += 1;
+  }
+
+  return array;
+}
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = fizzBuzz;
